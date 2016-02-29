287177395a8a662871692b75a6ddd314ea5666c0
Derick Gross <derick.gross@gmail.com>
2016-02-29 10:53:59 -0500

    updated twenty_one.rb

--- twenty_one.rb
+++ twenty_one.rb
@@ -19,18 +19,14 @@ def initialize_deck
   suits.product(ranks).shuffle!
 end
 
-def deal_card_to_player(player_cards, deck)
+def deal_card(player_cards, deck)
   player_cards << deck.pop
-end
-
-def deal_card_to_dealer(dealer_cards, deck)
-  dealer_cards << deck.pop
 end
 
 def deal_initial_hands(player_cards, dealer_cards, deck)
   2.times do
-    deal_card_to_player(player_cards, deck)
-    deal_card_to_dealer(dealer_cards, deck)
+    deal_card(player_cards, deck)
+    deal_card(dealer_cards, deck)
   end
 end
 
@@ -100,34 +96,41 @@ def twenty_one?(hand)
   hand_value(hand) == 21
 end
 
+def player_hits?
+  result = nil
+  loop do
+    prompt "Type 'hit' to hit or 'stay' to stay:"
+    player_action = gets.chomp
+
+    if player_action == 'hit'
+      result = true
+      break
+    elsif player_action == 'stay'
+      result = false
+      break
+    else
+      prompt "Invalid choice."
+      next
+    end
+  end
+  result
+end
+
 def player_bust(player_name, player_cards)
   display_cards(player_name, player_cards)
   prompt "#{player_name} busts with a hand value of #{hand_value(player_cards)}."
 end
 
-def player_turn(player_name, player_action, player_cards, deck)
-  loop do
-    deal_card_to_player(player_cards, deck)
-
-    break if twenty_one?(player_cards)
-    break if bust?(player_cards)
-
-    display_cards(player_name, player_cards)
-    display_hand_value(player_name, player_cards)
-    prompt "Type 'hit' to hit or 'stay' to stay:"
-    player_action = gets.chomp
-
-    break unless player_action == "hit"
-  end
-end
-
-def dealer_turn(dealer_cards, player_cards, deck, player_name)
+def dealer_hits(dealer_cards, player_cards, deck)
   while (hand_value(dealer_cards) < 17) || (hand_value(dealer_cards) < hand_value(player_cards))
     display_dealer_cards_and_hand_value(dealer_cards)
     prompt "Dealer hits."
-    deal_card_to_dealer(dealer_cards, deck)
+    deal_card(dealer_cards, deck)
     prompt "Dealer received the #{card_name(dealer_cards.last)}"
   end
+end
+
+def dealer_result(dealer_cards, player_cards, player_name)
   if hand_value(dealer_cards) > 21
     display_dealer_cards_and_hand_value(dealer_cards)
     prompt "Dealer busts.  #{player_name} wins!"
@@ -138,12 +141,52 @@ def dealer_turn(dealer_cards, player_cards, deck, player_name)
   end
 end
 
+def player_turn(player_name, player_action, player_cards, deck)
+  loop do
+    deal_card(player_cards, deck)
+
+    break if twenty_one?(player_cards) || bust?(player_cards)
+
+    display_cards(player_name, player_cards)
+    display_hand_value(player_name, player_cards)
+
+    break unless player_hits?
+  end
+end
+
+def dealer_turn(dealer_cards, player_cards, deck, player_name)
+  dealer_hits(dealer_cards, player_cards, deck)
+
+  dealer_result(dealer_cards, player_cards, player_name)
+end
+
+def play_again?
+  result = nil
+  loop do
+    prompt "Would you like to play again?  Type 'yes' or 'no'."
+
+    answer = gets.chomp
+    if answer.downcase == 'yes'
+      result = true
+      break
+    elsif answer.downcase == 'no'
+      result = false
+      break
+    else
+      prompt "Invalid choice."
+      next
+    end
+  end
+  result
+end
+
 prompt "Welcome to Twenty One!"
 
 prompt "What is the player's name?"
 player_name = gets.chomp
 
 loop do
+  set_empty_hands(player_cards, dealer_cards)
   deck = initialize_deck
   deal_initial_hands(player_cards, dealer_cards, deck)
 
@@ -152,15 +195,7 @@ loop do
 
   if twenty_one?(player_cards)
     prompt "#{player_name} wins!"
-    prompt "Would you like to play again?  Type 'yes' or 'no'."
-
-    answer = gets.chomp
-    if answer.downcase.start_with?('y')
-      set_empty_hands(player_cards, dealer_cards)
-      next
-    else
-      break
-    end
+    play_again? ? next : (prompt "Thank you for playing Twenty One!"; break)
   end
 
   prompt "You can only see one of the computer's cards: #{card_name(dealer_cards.first)}."
@@ -172,15 +207,7 @@ loop do
 
   if bust?(player_cards)
     player_bust(player_name, player_cards)
-    prompt "Would you like to play again?  Type 'yes' or 'no'."
-
-    answer = gets.chomp
-    if answer.downcase.start_with?('y')
-      set_empty_hands(player_cards, dealer_cards)
-      next
-    else
-      break
-    end
+    play_again? ? next : (prompt "Thank you for playing Twenty One!"; break)
   elsif twenty_one?(player_cards)
     display_hand_value(player_name, player_cards)
     prompt "#{player_name} wins!"
@@ -188,9 +215,5 @@ loop do
     dealer_turn(dealer_cards, player_cards, deck, player_name)
   end
 
-  set_empty_hands(player_cards, dealer_cards)
-  prompt "Would you like to play again?  Type 'yes' or 'no'."
-
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  play_again? ? next : (prompt "Thank you for playing Twenty One!"; break)
 end
